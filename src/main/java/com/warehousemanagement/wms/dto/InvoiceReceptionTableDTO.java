022a0c7ded24dca61f571adab3a777beb7ff63ef
Radvan Alexandru <rip777ton@gmail.com>
2023-07-04 22:36:49 +0300

    Create a dto class that is used to transfer the data for invoice table on the frontend

---
+++ src/main/java/com/warehousemanagement/wms/dto/InvoiceReceptionTableDTO.java
@@ -0,0 +1,111 @@
+package com.warehousemanagement.wms.dto;
+
+import java.util.Date;
+
+public class InvoiceReceptionTableDTO {
+    private Integer id;
+    private Boolean validated;
+    private Date dateOfCreation;
+    private Date dateOfValidation;
+    private String validatedBy;
+    private String createdBy;
+    private Double totalBuyingPrice;
+    private Double totalSellingPrice;
+    private String provider;
+
+    public InvoiceReceptionTableDTO() {
+    }
+
+    public InvoiceReceptionTableDTO(Integer id,
+                                    Boolean validated,
+                                    Date dateOfCreation,
+                                    Date dateOfValidation,
+                                    String validatedBy,
+                                    String createdBy,
+                                    Double totalBuyingPrice,
+                                    Double totalSellingPrice,
+                                    String provider) {
+        this.id = id;
+        this.validated = validated;
+        this.dateOfCreation = dateOfCreation;
+        this.dateOfValidation = dateOfValidation;
+        this.validatedBy = validatedBy;
+        this.createdBy = createdBy;
+        this.totalBuyingPrice = totalBuyingPrice;
+        this.totalSellingPrice = totalSellingPrice;
+        this.provider = provider;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Boolean getValidated() {
+        return validated;
+    }
+
+    public void setValidated(Boolean validated) {
+        this.validated = validated;
+    }
+
+    public Date getDateOfCreation() {
+        return dateOfCreation;
+    }
+
+    public void setDateOfCreation(Date dateOfCreation) {
+        this.dateOfCreation = dateOfCreation;
+    }
+
+    public Date getDateOfValidation() {
+        return dateOfValidation;
+    }
+
+    public void setDateOfValidation(Date dateOfValidation) {
+        this.dateOfValidation = dateOfValidation;
+    }
+
+    public String getValidatedBy() {
+        return validatedBy;
+    }
+
+    public void setValidatedBy(String validatedBy) {
+        this.validatedBy = validatedBy;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public Double getTotalBuyingPrice() {
+        return totalBuyingPrice;
+    }
+
+    public void setTotalBuyingPrice(Double totalBuyingPrice) {
+        this.totalBuyingPrice = totalBuyingPrice;
+    }
+
+    public Double getTotalSellingPrice() {
+        return totalSellingPrice;
+    }
+
+    public void setTotalSellingPrice(Double totalSellingPrice) {
+        this.totalSellingPrice = totalSellingPrice;
+    }
+
+    public String getProvider() {
+        return provider;
+    }
+
+    public void setProvider(String provider) {
+        this.provider = provider;
+    }
+
+}
