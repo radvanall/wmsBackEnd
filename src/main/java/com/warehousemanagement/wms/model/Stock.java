bb9acd8735d453a22574bd468734441be5872d94
Radvan Alexandru <rip777ton@gmail.com>
2023-07-19 21:01:59 +0300

    Add remainQuantity ,state fields to the stock. Remove CascadeType.ALL from the position field. Add invoiceRecepiton field in order to get access to invoiceReception’s keys from stock entity.

--- src/main/java/com/warehousemanagement/wms/model/Stock.java
+++ src/main/java/com/warehousemanagement/wms/model/Stock.java
@@ -14,17 +14,22 @@ public class Stock {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private Integer stockQuantity;
+    private Integer remainingQuantity;
     private Double buyingPrice;
     private Double sellingPrice;
+    private String state;
+
     @OneToMany(fetch = FetchType.LAZY,cascade = {CascadeType.ALL})
     @JoinColumn(name = "stock_id",referencedColumnName = "id")
     private List<Order> order;
 
-    @ManyToOne(fetch = FetchType.EAGER,cascade = {CascadeType.ALL})
+    @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "position_id",referencedColumnName = "id")
     private Position position;
-
-
+    @JsonIgnore
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "invoice_reception_id",referencedColumnName = "id")
+    InvoiceReception invoiceReception;
     public Stock() {
 
     }
@@ -39,9 +44,11 @@ public class Stock {
 
     public Stock( Integer stockQuantity, Double buyingPrice, Double sellingPrice, Position position) {
         this.stockQuantity = stockQuantity;
+        this.remainingQuantity=stockQuantity;
         this.buyingPrice = buyingPrice;
         this.sellingPrice = sellingPrice;
         this.position = position;
+        this.state="unvalidated";
     }
 
     public Stock(Integer stockQuantity, Double sellingPrice, Double buyingPrice, List<Order> order) {
@@ -55,6 +62,24 @@ public class Stock {
         this.stockQuantity = stockQuantity;
         this.sellingPrice = sellingPrice;
         this.buyingPrice=buyingPrice;
+    }
+
+    public Stock(Integer id,
+                 Integer stockQuantity,
+                 Integer remainingQuantity,
+                 Double buyingPrice,
+                 Double sellingPrice,
+                 String state,
+                 List<Order> order,
+                 Position position) {
+        this.id = id;
+        this.stockQuantity = stockQuantity;
+        this.remainingQuantity = remainingQuantity;
+        this.buyingPrice = buyingPrice;
+        this.sellingPrice = sellingPrice;
+        this.state = state;
+        this.order = order;
+        this.position = position;
     }
 
     public Integer getId() {
@@ -112,15 +137,42 @@ public class Stock {
         this.buyingPrice=stock.buyingPrice;
     }
 
+    public Integer getRemainingQuantity() {
+        return remainingQuantity;
+    }
+
+    public void setRemainingQuantity(Integer remainingQuantity) {
+        this.remainingQuantity = remainingQuantity;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public InvoiceReception getInvoiceReception() {
+        return invoiceReception;
+    }
+
+    public void setInvoiceReception(InvoiceReception invoiceReception) {
+        this.invoiceReception = invoiceReception;
+    }
+
     @Override
     public String toString() {
         return "Stock{" +
                 "id=" + id +
                 ", stockQuantity=" + stockQuantity +
+                ", remainingQuantity=" + remainingQuantity +
                 ", buyingPrice=" + buyingPrice +
                 ", sellingPrice=" + sellingPrice +
+                ", state='" + state + '\'' +
                 ", order=" + order +
-//                ", position=" + position +
+                ", position=" + position.getName() +
+                ", invoiceReception=" + invoiceReception +
                 '}';
     }
 }
