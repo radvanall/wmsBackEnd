fae7282b2f212546768e8bde4c7f699b2befb12b
Radvan Alexandru <rip777ton@gmail.com>
2023-07-24 20:29:56 +0300

    Create new methods that return the max buying,selling prices and max quantity from db.

--- src/main/java/com/warehousemanagement/wms/repository/StockRepository.java
+++ src/main/java/com/warehousemanagement/wms/repository/StockRepository.java
@@ -2,8 +2,17 @@ package com.warehousemanagement.wms.repository;
 
 import com.warehousemanagement.wms.model.Stock;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface StockRepository extends JpaRepository<Stock,Integer> {
+    @Query("SELECT MAX(stockQuantity) from Stock")
+    Integer findMaxQuantity();
+    @Query("SELECT MAX(buyingPrice) from Stock")
+    Double findMaxBuyingPrice();
+    @Query("SELECT MAX(sellingPrice) from Stock")
+    Double findMaxSellingPrice();
+
+
 }
