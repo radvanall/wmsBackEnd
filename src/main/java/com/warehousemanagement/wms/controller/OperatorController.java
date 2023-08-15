8d0d8350f33a227a2040bb60316cb6120974e61e
Radvan Alexandru <rip777ton@gmail.com>
2023-08-15 19:38:10 +0300

    Refactor the getOperator methods to return a dto object instead an Operator object. Create a setWorkedHours function that is used to set the operator's worked hours for a date.

--- src/main/java/com/warehousemanagement/wms/controller/OperatorController.java
+++ src/main/java/com/warehousemanagement/wms/controller/OperatorController.java
@@ -6,8 +6,11 @@ import com.warehousemanagement.wms.model.Operator;
 import com.warehousemanagement.wms.services.AdministratorService;
 import com.warehousemanagement.wms.services.OperatorService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
 //@CrossOrigin("*")
 @RestController
@@ -28,10 +31,14 @@ public class OperatorController {
         return operatorService.getOperatorTableDTOs();
     }
     @RequestMapping(method = RequestMethod.GET, value="/read/{id}")
-    public Operator getOperator(@PathVariable Integer id){
-        return operatorService.getOperator(id);
+    public ResponseEntity<?> getOperator(@PathVariable Integer id){
+        try {
+            return ResponseEntity.ok(operatorService.getOperator(id));
+        }catch(RuntimeException e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
+        }
 
-    }
     @RequestMapping(method = RequestMethod.PUT, value="/update/{id}")
     public void updateOperator(@RequestBody Operator operator, @PathVariable Integer id){
         operatorService.updateOperator(operator,id);
@@ -39,6 +46,12 @@ public class OperatorController {
     @RequestMapping(method = RequestMethod.PUT, value="/updateInvoices/{operatorId}")
     public void addInvoice(@RequestBody Operator operator, @PathVariable Integer operatorId){
         operatorService.addInvoice(operator,operatorId);
+    }
+    @RequestMapping(method = RequestMethod.POST, value="/setWorkedHours")
+    public ResponseEntity<?> setWorkedHours(@RequestParam("id") Integer id,
+                               @RequestParam("date") String date,
+                               @RequestParam("workedHours") Integer workedHours){
+        return  operatorService.setWorkedHours(id,date,workedHours);
     }
 
 }
--- src/main/java/com/warehousemanagement/wms/services/OperatorService.java
+++ src/main/java/com/warehousemanagement/wms/services/OperatorService.java
@@ -1,22 +1,41 @@
 package com.warehousemanagement.wms.services;
 
+import com.warehousemanagement.wms.dto.OperatorInvoiceDTO;
 import com.warehousemanagement.wms.dto.OperatorTableDTO;
+import com.warehousemanagement.wms.dto.SingleOperatorDTO;
+import com.warehousemanagement.wms.dto.WorkDaysDTO;
 import com.warehousemanagement.wms.model.Administrator;
 import com.warehousemanagement.wms.model.Invoice;
 import com.warehousemanagement.wms.model.Operator;
+import com.warehousemanagement.wms.model.OperatorWorkDays;
 import com.warehousemanagement.wms.repository.InvoiceRepository;
 import com.warehousemanagement.wms.repository.OperatorRepository;
+import com.warehousemanagement.wms.utils.CompareDateDMY;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class OperatorService {
     @Autowired
     private OperatorRepository operatorRepository;
-
+    public OperatorWorkDays findDay(List<OperatorWorkDays> workDays,Date date){
+        for(OperatorWorkDays workDay:workDays){
+            if(CompareDateDMY.compareDates(workDay.getData(),date)){
+                return workDay;
+            }
+        }
+        return  null;
+    }
     public void setOperator(List<Operator> operatorsList) {
         operatorRepository.saveAll(operatorsList);
     }
@@ -31,8 +50,34 @@ public class OperatorService {
         }
         return operatorTableDTOs;
     }
-    public Operator getOperator(Integer id){
-        return operatorRepository.findById(id).get();
+    public SingleOperatorDTO getOperator(Integer id){
+        Optional<Operator> optionalOperator = operatorRepository.findById(id);
+        if(!optionalOperator.isPresent())
+            throw new RuntimeException("Operator not found with id: " + id);
+        Operator operator=optionalOperator.get();
+        operator.getOperatorWorkDays().stream().forEach(day->
+                System.out.println(day.toString()));
+        SingleOperatorDTO operatorDTO=new SingleOperatorDTO(
+                operator.getId(),
+                operator.getNickname(),
+                operator.getAvatar(),
+                operator.getPhone(),
+                operator.getEmail(),
+                operator.getAddress(),
+                operator.getName(),
+                operator.getSurname(),
+                operator.getStatus(),
+                operator.getOperatorWorkDays().stream().map(day->
+                    new WorkDaysDTO(day.getId(),day.getData(),day.getWorkedHours())
+                ).collect(Collectors.toList()),
+                operator.getInvoices().stream().map(invoice ->
+                        new OperatorInvoiceDTO(invoice.getId(),invoice.getDate(),
+                                invoice.getCustomer().getNickname(),invoice.getShipped(),invoice.getTotalPrice()))
+                .collect(Collectors.toList())
+        );
+        operatorDTO.getWorkedDays().stream().forEach(day->
+                System.out.println(day.toString()));
+        return operatorDTO;
     }
     public void updateOperator(Operator operator,Integer id){
         Operator getOperator=operatorRepository.findById(id).get();
@@ -57,5 +102,40 @@ public class OperatorService {
         operatorTableDTO.setTel(operator.getPhone());
 
         return operatorTableDTO;
+    }
+
+    public ResponseEntity<?> setWorkedHours(Integer id, String date, Integer workedHours) {
+        try {
+            Date today=new Date();
+            Date newDate = new SimpleDateFormat("dd.MM.yyyy").parse(date);
+            System.out.println("id=" + id);
+            System.out.println("date=" + newDate);
+            System.out.println("workedHours=" + workedHours);
+           System.out.println(CompareDateDMY.compareDates(today,newDate));
+
+            Optional<Operator> optionalOperator=operatorRepository.findById(id);
+            if(!optionalOperator.isPresent()) return  ResponseEntity.badRequest().body("Operatorul nu exista");
+            Operator operator=optionalOperator.get();
+            List<OperatorWorkDays> operatorWorkDays=operator.getOperatorWorkDays();
+            OperatorWorkDays foundDay=findDay(operatorWorkDays,newDate );
+            System.out.println("foundDate=" + foundDay);
+            if(foundDay==null){
+                operatorWorkDays.add(new OperatorWorkDays(newDate,workedHours));
+            }
+            else {
+                foundDay.setWorkedHours(workedHours);
+            }
+            operator.setOperatorWorkDays(operatorWorkDays);
+            operatorRepository.save(operator);
+//            operatorWorkDays.add(new OperatorWorkDays(newDate,workedHours));
+//            operator.setOperatorWorkDays(operator.);
+            return ResponseEntity.ok().body("datele au fost adaugate");
+        }catch(ParseException e){
+            return ResponseEntity.badRequest().body("Data nu e in format corect");
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred: " + e.getMessage());
+        }
+
+
     }
 }
