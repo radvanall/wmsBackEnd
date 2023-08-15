31797ef41eaa38ff93fdd23fb2eebb8df1340398
Radvan Alexandru <rip777ton@gmail.com>
2023-08-15 19:50:18 +0300

    Add workedHours field to the class.

--- src/main/java/com/warehousemanagement/wms/model/OperatorWorkDays.java
+++ src/main/java/com/warehousemanagement/wms/model/OperatorWorkDays.java
@@ -1,5 +1,7 @@
 package com.warehousemanagement.wms.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import javax.persistence.*;
 import java.util.Date;
 
@@ -11,13 +13,17 @@ public class OperatorWorkDays {
     private Integer id;
     @Temporal(TemporalType.TIMESTAMP)
     private Date data;
+    private Integer workedHours;
 
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
     public Date getData() {
         return data;
     }
 
-    public void setData(Date data) {
+
+    public OperatorWorkDays( Date data, Integer workedHours) {
         this.data = data;
+        this.workedHours = workedHours;
     }
 
     public OperatorWorkDays() {
@@ -25,6 +31,35 @@ public class OperatorWorkDays {
 
     public OperatorWorkDays(Date data) {
         this.data = data;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    public Integer getWorkedHours() {
+        return workedHours;
+    }
+
+    public void setWorkedHours(Integer workedHours) {
+        this.workedHours = workedHours;
+    }
+
+    @Override
+    public String toString() {
+        return "OperatorWorkDays{" +
+                "id=" + id +
+                ", data=" + data.toString() +
+                ", workedHours=" + workedHours +
+                '}';
     }
 
     public void copyDay(OperatorWorkDays operatorWorkDay) {
