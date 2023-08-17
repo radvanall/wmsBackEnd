1e1b775dab06630eedb184b7a4311aec11348a68
Radvan Alexandru <rip777ton@gmail.com>
2023-08-17 22:12:51 +0300

    Create a findByNickName jpa method for the Administrator.

--- src/main/java/com/warehousemanagement/wms/repository/AdministratorRepository.java
+++ src/main/java/com/warehousemanagement/wms/repository/AdministratorRepository.java
@@ -4,6 +4,9 @@ import com.warehousemanagement.wms.model.Administrator;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface AdministratorRepository extends JpaRepository<Administrator,Integer> {
+   Optional<Administrator> findByNickname(String username);
 }
