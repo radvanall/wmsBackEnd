fbabfde95f755814b4321bcb167a893016447b7b
Radvan Alexandru <rip777ton@gmail.com>
2023-08-17 22:09:11 +0300

    Create a class that transfers the sales and aquisitions made in a period of time.

---
+++ src/main/java/com/warehousemanagement/wms/utils/SalesAndAcquisitions.java
@@ -0,0 +1,32 @@
+package com.warehousemanagement.wms.utils;
+
+import com.warehousemanagement.wms.dto.SaleAndAcquisitionDTO;
+import com.warehousemanagement.wms.dto.WeeklySalesDTO;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+public class SalesAndAcquisitions {
+    public static List<SaleAndAcquisitionDTO> getSalesAndAcquisitions(Calendar monthsAgo,
+                                                                     List<WeeklySalesDTO> salesDTOS,
+                                                                     List<WeeklySalesDTO> acquisitionsDTOS){
+        Calendar currentDate = Calendar.getInstance();
+        List<SaleAndAcquisitionDTO> saleAndAcquisitionDTOS=new ArrayList<>();
+        while (currentDate.after(monthsAgo)) {
+            Calendar weekStart = (Calendar) currentDate.clone();
+            weekStart.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY);
+            weekStart.set(Calendar.HOUR_OF_DAY, 0);
+            weekStart.set(Calendar.MINUTE, 0);
+            weekStart.set(Calendar.SECOND, 0);
+            Calendar weekEnd = (Calendar) weekStart.clone();
+            weekEnd.add(Calendar.DAY_OF_WEEK, 6);
+            System.out.println("weekStart="+weekStart.getTime());
+            double sumSales = FindSumForDate.findSumForDate(salesDTOS, weekStart.getTime(),weekEnd.getTime());
+            double sumAcquisitions = FindSumForDate.findSumForDate(acquisitionsDTOS, weekStart.getTime(),weekEnd.getTime());
+            saleAndAcquisitionDTOS.add(new SaleAndAcquisitionDTO(weekStart.getTime(),sumSales,sumAcquisitions));
+            currentDate.add(Calendar.WEEK_OF_YEAR, -1);
+        }
+        return saleAndAcquisitionDTOS;
+    }
+}
