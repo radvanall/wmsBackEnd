6092a6cd4b9aab361a15a55e6e52833320d756a7
Radvan Alexandru <rip777ton@gmail.com>
2023-08-24 21:35:29 +0300

    Add new fields and constructors.

--- src/main/java/com/warehousemanagement/wms/model/AdminWorkDays.java
+++ src/main/java/com/warehousemanagement/wms/model/AdminWorkDays.java
@@ -1,5 +1,7 @@
 package com.warehousemanagement.wms.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import javax.persistence.*;
 import java.util.Date;
 
@@ -9,18 +11,30 @@ public class AdminWorkDays {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    private Integer workedHours;
     @Temporal(TemporalType.TIMESTAMP)
     private Date data;
 
+    public AdminWorkDays(Date newDate, Integer workedHours) {
+        this.data=newDate;
+        this.workedHours=workedHours;
+    }
+
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
     public Date getData() {
         return data;
+    }
+    public AdminWorkDays() {
+    }
+
+    public AdminWorkDays(Integer id, Integer workedHours, Date data) {
+        this.id = id;
+        this.workedHours = workedHours;
+        this.data = data;
     }
 
     public void setData(Date data) {
         this.data = data;
-    }
-
-    public AdminWorkDays() {
     }
 
     public AdminWorkDays(Date data) {
@@ -29,5 +43,21 @@ public class AdminWorkDays {
 
     public void copyDay(AdminWorkDays getDay) {
         this.data=getDay.data;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getWorkedHours() {
+        return workedHours;
+    }
+
+    public void setWorkedHours(Integer workedHours) {
+        this.workedHours = workedHours;
     }
 }
