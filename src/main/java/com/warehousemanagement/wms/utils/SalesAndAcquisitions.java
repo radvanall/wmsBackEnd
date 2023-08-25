9b7a79c3ca365f4c147d3500eca5163e7535dcfa
Radvan Alexandru <rip777ton@gmail.com>
2023-08-25 21:33:10 +0300

    Add 2 new methods.

--- src/main/java/com/warehousemanagement/wms/utils/SalesAndAcquisitions.java
+++ src/main/java/com/warehousemanagement/wms/utils/SalesAndAcquisitions.java
@@ -29,4 +29,48 @@ public class SalesAndAcquisitions {
         }
         return saleAndAcquisitionDTOS;
     }
+    public static List<SaleAndAcquisitionDTO> getValidSalesAndAcquisitions(Calendar monthsAgo,
+                                                                      List<WeeklySalesDTO> salesDTOS,
+                                                                      List<WeeklySalesDTO> acquisitionsDTOS){
+        Calendar currentDate = Calendar.getInstance();
+        List<SaleAndAcquisitionDTO> saleAndAcquisitionDTOS=new ArrayList<>();
+        while (currentDate.after(monthsAgo)) {
+            Calendar weekStart = (Calendar) currentDate.clone();
+            weekStart.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY);
+            weekStart.set(Calendar.HOUR_OF_DAY, 0);
+            weekStart.set(Calendar.MINUTE, 0);
+            weekStart.set(Calendar.SECOND, 0);
+            Calendar weekEnd = (Calendar) weekStart.clone();
+            weekEnd.add(Calendar.DAY_OF_WEEK, 6);
+            System.out.println("weekStart="+weekStart.getTime());
+            double sumSales = FindSumForDate.findSumForDate(salesDTOS, weekStart.getTime(),weekEnd.getTime());
+            double sumAcquisitions = FindSumForDate.findSumForDate(acquisitionsDTOS, weekStart.getTime(),weekEnd.getTime());
+            if(sumAcquisitions!=0||sumSales!=0)
+            saleAndAcquisitionDTOS.add(new SaleAndAcquisitionDTO(weekStart.getTime(),sumSales,sumAcquisitions));
+            currentDate.add(Calendar.WEEK_OF_YEAR, -1);
+        }
+        return saleAndAcquisitionDTOS;
+    }
+
+    public static List<WeeklySalesDTO> getWeeklyBalance(Calendar monthsAgo,
+                                                                      List<WeeklySalesDTO> salesDTOS,
+                                                                      List<WeeklySalesDTO> acquisitionsDTOS){
+        Calendar currentDate = Calendar.getInstance();
+        List<WeeklySalesDTO> weeklyBalance=new ArrayList<>();
+        while (currentDate.after(monthsAgo)) {
+            Calendar weekStart = (Calendar) currentDate.clone();
+            weekStart.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY);
+            weekStart.set(Calendar.HOUR_OF_DAY, 0);
+            weekStart.set(Calendar.MINUTE, 0);
+            weekStart.set(Calendar.SECOND, 0);
+            Calendar weekEnd = (Calendar) weekStart.clone();
+            weekEnd.add(Calendar.DAY_OF_WEEK, 6);
+            System.out.println("weekStart="+weekStart.getTime());
+            double sumSales = FindSumForDate.findSumForDate(salesDTOS, weekStart.getTime(),weekEnd.getTime());
+            double sumAcquisitions = FindSumForDate.findSumForDate(acquisitionsDTOS, weekStart.getTime(),weekEnd.getTime());
+            weeklyBalance.add(new WeeklySalesDTO(weekStart.getTime(),sumSales-sumAcquisitions));
+            currentDate.add(Calendar.WEEK_OF_YEAR, -1);
+        }
+        return weeklyBalance;
+    }
 }
