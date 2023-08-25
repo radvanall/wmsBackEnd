7486c8d463eee8eddd1dd18984e1c5c0effc7dae
Radvan Alexandru <rip777ton@gmail.com>
2023-08-25 21:29:49 +0300

    Add getBalance method to SaleAndAcquisitionDTO class.

--- src/main/java/com/warehousemanagement/wms/dto/SaleAndAcquisitionDTO.java
+++ src/main/java/com/warehousemanagement/wms/dto/SaleAndAcquisitionDTO.java
@@ -40,4 +40,8 @@ public class SaleAndAcquisitionDTO {
         this.totalAcquisitions = totalAcquisitions;
     }
 
+    public double getBalance(){
+        return totalSales-totalAcquisitions;
+    }
+
 }
